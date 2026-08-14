8ebeb9e7eaa3e79ec11cbe0f82910b3767cad911
Boxchan <twinbox6@gmail.com>
2026-08-15 00:22:43 +0800

    添加了fst波形支持，并新添了make view规则

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,5 +1,6 @@
 #include "Vtop.h"
 #include "verilated.h"
+#include "verilated_fst_c.h"
 
 #include <cassert>
 #include <iostream>
@@ -8,19 +9,27 @@
 
 int main(int argc,char* argv[]){
 	Verilated::commandArgs(argc,argv);
+	Verilated::traceEverOn(true);
 	Vtop* top = new Vtop;
+	VerilatedFstC* tfp = new VerilatedFstC;
+	top->trace(tfp, 99);
+	tfp->open("dump.fst");
+
 	srand(time(NULL));
 
 	for(int i = 0; i < 50; i++){
 		top->a = rand() & 1;
 		top->b = rand() & 1;
 		top->eval();
-		assert(top->result == top->a ^ top->b);
-        std::cout << "a="  << +top->a 
+		tfp->dump(i);
+		assert(top->result == (top->a ^ top->b));
+		std::cout << "a="  << +top->a 
      	          << " b=" << +top->b 
                   << " result=" << (int)top->result 
                   << std::endl;
 	}
+	tfp->close();
 	delete top;
+	delete tfp;
 	return 0;
 }
